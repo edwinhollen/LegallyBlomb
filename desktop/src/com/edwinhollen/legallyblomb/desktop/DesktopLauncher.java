6bfae53e3cf943c7da9680e49a9e01a6aa4f3ddd
Edwin Hollen <edwinhollen@gmail.com>
2016-09-17 20:05:59 -0400

    Add icons

--- desktop/src/com/edwinhollen/legallyblomb/desktop/DesktopLauncher.java
+++ desktop/src/com/edwinhollen/legallyblomb/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.edwinhollen.legallyblomb.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.edwinhollen.legallyblomb.LegallyBlomb;
@@ -7,6 +8,10 @@ import com.edwinhollen.legallyblomb.LegallyBlomb;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "LegallyBlomb";
+		config.addIcon("icon-128.png", Files.FileType.Internal);
+		config.addIcon("icon-32.png", Files.FileType.Internal);
+		config.addIcon("icon-16.png", Files.FileType.Internal);
 		new LwjglApplication(new LegallyBlomb(), config);
 	}
 }
